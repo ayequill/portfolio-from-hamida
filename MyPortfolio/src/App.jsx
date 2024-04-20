ef1d6d562573960738bcdeee8e48385105f06630
Mahama Hamida <mahamahamida@gmail.com>
2024-04-20 22:46:55 +0000

    add main app

---
+++ MyPortfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './components/NavBar.jsx'
+import NavBar from './components/NavBar.jsx'
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    </>
+  )
+}
+
+export default App
