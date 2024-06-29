9fd277bd00dd3965fd3a0a851356450d5a7082d7
Mahama Hamida <mahamahamida@gmail.com>
2024-06-29 23:47:19 +0000

    chore: update portfolio colors in tailwind.config.js

--- MyPortfolio/tailwind.config.js
+++ MyPortfolio/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "portfolioPrimary": "#0098D0",
+        "portfolioSecondary": "#0056B8",
+        "portfolioTextDark": "#2a2a2a",
+        "portfolioTextLight": "#656560",
+        "portfolioStroke": "#CAD0D8",
+        "portfolioWhite": "#ffffff",
+        "portfolioGradient": "linear-gradient(90deg,#5FD4FF 0%, #0056B8 100% )"
+      }
+    },
   },
   plugins: [],
 }
