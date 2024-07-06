c6e3e1e96cca50732095d1db9e1a942ef6d48c87
Mahama Hamida <mahamahamida@gmail.com>
2024-07-06 23:20:53 +0000

    feat: Add lazy loading for images in Works component

--- MyPortfolio/src/components/Works.jsx
+++ MyPortfolio/src/components/Works.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Card from "./Card";
 
 export default function Works() {
   const [isActive, setActive] = useState("Design");
@@ -10,28 +11,35 @@ export default function Works() {
   return (
     <section className="text-portfolioTextDark w-screen py-16 xl:py-20 px-4">
       <div className="flex gap-8 flex-col max-w-3xl mx-auto justify-center items-center">
+        <p className="text-3xl xl:text-[40px] font-bold">Work</p>
+        <p>A showcase of my proudest creations</p>
+        <div className="text-portfolioTextLight flex w-auto max-w-[270px] bg-gray-300 gap-3 p-1 rounded-full ">
+          <button
+            type="button"
+            className={`py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Design" ? "bg-portfolioWhite text-portfolioTextDark font-semibold " : ""}`}
+            onClick={() => handleActive("Design")}
+            // aria-current="page"
+          >
+            Design
+          </button>
 
-      <p className="text-3xl xl:text-[40px] font-bold">Work</p>
-      <div className="text-portfolioTextLight flex w-auto max-w-[270px] bg-gray-300 gap-3 p-1 rounded-full ">
-        <button
-          type="button"
-          className={`py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Design" ? "bg-portfolioWhite text-portfolioTextDark font-semibold " : ""}`}
-          onClick={() => handleActive("Design")}
-          aria-current="page"
-        >
-          Design
-        </button>
-        
-        <button
-          type="button"
-          className={` py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Development" ? "bg-portfolioWhite text-portfolioTextDark font-semibold " : ""}`}
-          onClick={() => handleActive("Development")}
-        >
-          Development
-        </button>
-      </div>
-      {isActive === "Design" && <p>render this when Design is selected</p>}
-      {isActive === "Development" && <p>Render this when development is selected</p>}
+          <button
+            type="button"
+            className={` py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Development" ? "bg-portfolioWhite text-portfolioTextDark font-semibold " : ""}`}
+            onClick={() => handleActive("Development")}
+          >
+            Development
+          </button>
+        </div>
+        {isActive === "Design" && (
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 w-full">
+            <Card />
+            <Card />
+          </div>
+        )}
+        {isActive === "Development" && (
+          <p>Render this when development is selected</p>
+        )}
       </div>
     </section>
   );
