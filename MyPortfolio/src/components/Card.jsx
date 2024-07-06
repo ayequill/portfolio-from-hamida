3f946018e9102a0d50b043bb1d298eca24d6b51a
Mahama Hamida <mahamahamida@gmail.com>
2024-07-06 23:20:35 +0000

    feat: Add Card component for displaying portfolio items

---
+++ MyPortfolio/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import curiosity from "../assets/thumbnails/curiosity.png"
+import ProductModal from "./productModal";
+
+export default function Card() {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => setModalOpen(false);
+    return(
+        <>
+        <div className="bg-gray-600 max-w-sm w-full h-auto rounded-2xl overflow-hidden"
+        onClick={openModal}>
+            <img className="w-full h-full object-cover transition-transform duration-200 hover:scale-105" src={curiosity} />
+        </div>
+        <ProductModal isOpen={isModalOpen} onClose={closeModal} />
+            </>
+
+    )
+}
