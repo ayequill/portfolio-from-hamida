a26a40f0a9444d6d11f394cae42b5c54b87d0aa0
Mahama Hamida <mahamahamida@gmail.com>
2024-07-06 00:14:22 +0000

    feat: Add Works component to display works section on the portfolio page

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import HeroSection from './components/Hero.jsx'
 import './components/NavBar.jsx'
 import NavBar from './components/NavBar.jsx'
+import Works from './components/Works.jsx'
 
 function App() {
   return (
     <>
     <NavBar />
     <HeroSection />
+    <Works />
     </>
   )
 }
