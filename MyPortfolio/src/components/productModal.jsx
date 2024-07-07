6c3e7b3eadd3729722b8b4257c8157e7d6470e58
Mahama Hamida <mahamahamida@gmail.com>
2024-07-07 15:26:58 +0000

    refactor: Update ProductModal component to improve layout and content

--- MyPortfolio/src/components/productModal.jsx
+++ MyPortfolio/src/components/productModal.jsx
@@ -7,7 +7,7 @@ export default function ProductModal({ isOpen, onClose }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-50 z-50 px-4">
-      <div className="relative bg-portfolioWhite max-w-5xl h-[500px] w-full rounded-3xl  overflow-hidden">
+      <div className="relative bg-portfolioWhite max-w-5xl h-3/4 w-full rounded-3xl  overflow-hidden">
         {/* <div className="flex flex-row-reverse justify-between items-center p">
           <span className="sr-only">Modal title</span>
           <CloseCircle
@@ -21,7 +21,7 @@ export default function ProductModal({ isOpen, onClose }) {
           <div className="h-full ">
             <img src={curiosity} className=" w-full h-full object-cover" />
           </div>
-          <div className="pb-4 pt-20 px-8 text-left text-portfolioTextLight">
+          <div className="pb-4 pt-8 lg:pt-20 px-8 text-left text-portfolioTextLight">
             <CloseCircle
               onClick={onClose}
               className="cursor-pointer absolute top-5 right-5 icon-hover"
@@ -29,25 +29,33 @@ export default function ProductModal({ isOpen, onClose }) {
               color="#656560"
             />
             <div className="flex flex-col gap-12">
-            <div className="flex flex-col gap-3">
-
-            <p className="font-bold text-left text-portfolioTextDark">
-              App for a Remote water quality monitoring systems
-            </p>
-            <p>
-              This is different from my other projects as it was an app for an
-              IoT system we built for a remote water quality assessment for fish
-              farmers.
-            </p>
-            </div>
-            <div className="flex gap-3 flex-wrap">
-              <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">Internet of Things</p>
-              <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">product design</p>
-              <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">fish</p>
-              <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">fish farmers</p>
-              <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">water</p>
-            
-            </div>
+              <div className="flex flex-col gap-3">
+                <p className="font-bold text-left text-portfolioTextDark">
+                  App for a Remote water quality monitoring systems
+                </p>
+                <p>
+                  This project stands out from my previous work because it
+                  involved developing an app for an IoT system designed to
+                  remotely assess water quality for fish farmers.
+                </p>
+              </div>
+              <div className="flex gap-3 flex-wrap">
+                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  Internet of Things
+                </p>
+                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  product design
+                </p>
+                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  fish
+                </p>
+                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  fish farmers
+                </p>
+                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  water
+                </p>
+              </div>
             </div>
           </div>
         </div>
