1d9e0a39fdb59dbe861b6992b37e5c56afa564d9
Mahama Hamida <mahamahamida@gmail.com>
2024-07-14 12:57:55 +0000

    refactor: Update Card component to use lazy loading for images

--- MyPortfolio/src/components/Card.jsx
+++ MyPortfolio/src/components/Card.jsx
@@ -1,43 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import curiosity from "../assets/thumbnails/test.png";
 import ProductModal from "./productModal";
-import {fetchProjects} from "../lib/api";
+import PropTypes from "prop-types";
 
-export default function Card() {
-  const [projects, setProject] = useState([]);
-  
-  useEffect(() => {
-    const getProjects = async () => {
-        const fetchedProjects = await fetchProjects();
-        if (fetchedProjects) {
-          setProject(fetchedProjects.data);
-        }
-    }
-    getProjects();
-  }, []);
+// export default function Card({ thumbnail, altText, id }) {
+  export default function Card({ project }) {
 
   const [isModalOpen, setModalOpen] = useState(false);
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
+  const { title, thumbnail, thumbnailGif, description, id } = project;
+
   return (
     <>
-      <div>
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="bg-gray-600 max-w-sm w-full max-h-64 h-auto rounded-3xl overflow-hidden mb-4"
-            onClick={openModal}
-          >
-            <img
-              className="w-full h-full object-contain transition-transform duration-200 hover:scale-105"
-              src={`http://localhost:1337${project.attributes.thumbnail.data.attributes.url}`}
-              alt={project.attributes.thumbnail.data.attributes.alternativeText || 'Thumbnail'}
-            />
-            {/* <p className="text-white text-center mt-2">{project.attributes.title}</p> */}
+      <div
+        key={id}
+        className="bg-gray-600 max-w-sm w-full max-h-64 h-auto rounded-3xl overflow-hidden mb-4"
+        onClick={openModal}
+      >
+        {thumbnail ? (
+          <img
+            className="w-full h-full object-cover transition-transform duration-200 hover:scale-105"
+            src={`http://localhost:1337${project.thumbnail.url}`}
+            alt={`http://localhost:1337${project.thumbnail.altText}` || 'Thumbnail'}
+          />
+        ) : (
+          <div className="w-full h-full flex items-center justify-center text-white">
+            No Thumbnail Available
           </div>
-        ))}
+        )}
       </div>
-      <ProductModal isOpen={isModalOpen} onClose={closeModal} />
+      <ProductModal isOpen={isModalOpen} onClose={closeModal} projectTitle={project.title} projectDescription={project.description} thumbnailGif={`http://localhost:1337${project.thumbnailGif.url}`} />
     </>
   );
 }
+
+// Card.propTypes = {
+//   thumbnail: PropTypes.string,
+//   altText: PropTypes.string,
+//   id: PropTypes.number,
+// };
+
+Card.propTypes = {
+  project: PropTypes.object.isRequired,
+};
