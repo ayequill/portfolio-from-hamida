2f5219e3f89006431e5daf3a02887d2b2f478528
Mahama Hamida <mahamahamida@gmail.com>
2024-07-14 12:58:24 +0000

    feat: Add lazy loading for images in Works component

--- MyPortfolio/src/components/Works.jsx
+++ MyPortfolio/src/components/Works.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import { fetchProjects } from "../lib/api";
 
 export default function Works() {
+  const [projects, setProjects] = useState([]);
   const [isActive, setActive] = useState("Design");
+
+  useEffect(() => {
+    const getProjects = async () => {
+      const fetchedProjects = await fetchProjects("/api/works");
+      console.log(fetchedProjects.data);
+      if (fetchedProjects) {
+        setProjects(fetchedProjects.data);
+      }
+    };
+    getProjects();
+  }, []);
 
   const handleActive = (button) => {
     setActive(button);
@@ -13,10 +26,14 @@ export default function Works() {
       <div className="flex gap-8 flex-col max-w-3xl mx-auto justify-center items-center">
         <p className="text-3xl xl:text-[40px] font-bold">Work</p>
         <p>A showcase of my proudest creations</p>
-        <div className="text-portfolioTextLight flex w-full max-w-[270px] bg-gray-300 gap-3 p-[1px] rounded-full  ">
+        <div className="text-portfolioTextLight flex w-full max-w-[270px] bg-gray-300 gap-3 p-[1px] rounded-full">
           <button
             type="button"
-            className={`py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Design" ? "bg-portfolioWhite buttonGroup-shadow text-portfolioTextDark font-semibold " : ""}`}
+            className={`py-2 px-6 transition duration-300 ease-in-out rounded-full ${
+              isActive === "Design"
+                ? "bg-portfolioWhite buttonGroup-shadow text-portfolioTextDark font-semibold"
+                : ""
+            }`}
             onClick={() => handleActive("Design")}
           >
             Design
@@ -24,16 +41,29 @@ export default function Works() {
 
           <button
             type="button"
-            className={` py-2 px-6 transition duration-300 ease-in-out rounded-full ${isActive === "Development" ? "bg-portfolioWhite buttonGroup-shadow text-portfolioTextDark font-semibold " : ""}`}
+            className={`py-2 px-6 transition duration-300 ease-in-out rounded-full ${
+              isActive === "Development"
+                ? "bg-portfolioWhite buttonGroup-shadow text-portfolioTextDark font-semibold"
+                : ""
+            }`}
             onClick={() => handleActive("Development")}
           >
             Development
           </button>
         </div>
         {isActive === "Design" && (
-          <div className="grid grid-cols-1 xl:grid-cols-2 gap-8  w-full">
-            <Card />
-            <Card />
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 w-full">
+            {projects.map((project) => (
+              <Card
+                key={project.id}
+                // id={project.id}
+                // thumbnail={`http://localhost:1337${project?.[0]?.thumbnail?.url}`}
+                // altText={`http://localhost:1337${project?.[0]?.thumbnail?.altText}`}
+                // thumbnail={`http://localhost:1337${project.thumbnail.url}`}
+                project={project}
+
+              />
+            ))}
           </div>
         )}
         {isActive === "Development" && (
