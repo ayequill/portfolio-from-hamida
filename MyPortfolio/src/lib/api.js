374a7a994328690b824ae145e06139a111c69a34
Mahama Hamida <mahamahamida@gmail.com>
2024-07-20 21:59:31 +0000

    refactor: Update API fetchProjects function to use query parameters and switch base URL to localhost

--- MyPortfolio/src/lib/api.js
+++ MyPortfolio/src/lib/api.js
@@ -14,7 +14,9 @@ const projectQuery = qs.stringify({
 });
 
 const fetchProjects = async (path) => {
-  const baseUrl = "https://hamida-mahama.onrender.com/";
+  // const baseUrl = "https://hamida-mahama.onrender.com/";
+  const baseUrl = "http://localhost:1337";
+
   const url = new URL(path, baseUrl);
   url.search = projectQuery;
   console.log(url.href);
