24471a5e425739f9564e4bf38cd7efb403261a9a
Mahama Hamida <mahamahamida@gmail.com>
2024-07-20 21:59:51 +0000

    refactor: Update ProductModal component to include dynamic project tags

--- MyPortfolio/src/components/productModal.jsx
+++ MyPortfolio/src/components/productModal.jsx
@@ -1,29 +1,16 @@
-import React from "react";
 import { CloseCircle } from "iconsax-react";
-import curiosity from "../assets/thumbnails/curiosityThumb.gif";
 import PropTypes from "prop-types";
 
 
-export default function ProductModal({ isOpen, onClose, projectVideo, projectTitle, projectDescription }) {
+export default function ProductModal({ isOpen, onClose, projectVideo, projectTitle, projectDescription,projectTags }) {
   if (!isOpen) return null;
-  
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-50 z-50 px-4">
       <div className="relative bg-portfolioWhite max-w-5xl h-3/4 w-full rounded-3xl  overflow-hidden">
-        {/* <div className="flex flex-row-reverse justify-between items-center p">
-          <span className="sr-only">Modal title</span>
-          <CloseCircle
-            onClick={onClose}
-            className="cursor-pointer"
-            size="32"
-            color="#656560"
-          />
-        </div> */}
         <div className="absolute grid grid-cols-1 xl:grid-cols-2 h-full max-h-full">
           <div className="h-full ">
             <img 
             src={projectVideo}
-            // src={`http://localhost:1337${project.thumbnailGif.url}`}
             className=" w-full h-full object-cover" />
           </div>
           <div className="pb-4 pt-8 lg:pt-20 px-8 text-left text-portfolioTextLight">
@@ -47,21 +34,13 @@ export default function ProductModal({ isOpen, onClose, projectVideo, projectTit
                 </p>
               </div>
               <div className="flex gap-3 flex-wrap">
-                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
-                  Internet of Things
+                {projectTags.map((tag, index) => (
+
+                <p key={index} className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
+                  {tag}
                 </p>
-                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
-                  product design
-                </p>
-                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
-                  fish
-                </p>
-                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
-                  fish farmers
-                </p>
-                <p className="py-1 px-2 border border-gray-300 text-portfolioTextDark rounded-md text-center text-[12px]">
-                  water
-                </p>
+                ))}
+                
               </div>
             </div>
           </div>
@@ -76,5 +55,6 @@ ProductModal.propTypes = {
   onClose: PropTypes.func,
   title: PropTypes.string.isRequired,
   projectVideo: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  projectTags: PropTypes.arrayOf(PropTypes.string),
 };
