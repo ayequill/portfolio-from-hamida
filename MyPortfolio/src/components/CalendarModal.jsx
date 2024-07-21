32b411582f5e0ec12dadefb1392884d6d3a1aad5
Mahama Hamida <mahamahamida@gmail.com>
2024-07-21 17:06:00 +0000

    refactor: Update CalendarModal component styles

--- MyPortfolio/src/components/CalendarModal.jsx
+++ MyPortfolio/src/components/CalendarModal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black backdrop-blur-sm bg-opacity-50 z-50 px-4">
-      <div className="relative bg-portfolioWhite max-w-5xl h-3/4 w-full rounded-3xl  overflow-hidden">
+      <div className="relative bg-portfolioWhite max-w-5xl h-3/4 w-full rounded-3xl overflow-hidden pt-4 md:px-12">
       <CloseCircle
               onClick={onClose}
               className="cursor-pointer absolute top-5 right-5 icon-hover"
