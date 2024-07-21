58b08171c55744d64e1ea2f931adf423e7e9d194
Mahama Hamida <mahamahamida@gmail.com>
2024-07-21 17:26:48 +0000

    refactor: Update NavBar component to fix sticky behavior and improve navigation links

--- MyPortfolio/src/components/NavBar.jsx
+++ MyPortfolio/src/components/NavBar.jsx
@@ -18,11 +18,10 @@ export default function NavBar() {
   return (
     <>
       <header className="w-screen backdrop-blur-sm xl:sticky z-20 bottom-0 fixed xl:top-0 px-4 bg-portfolioWhite backdrop-filter bg-opacity-50 pb-8 xl:pt-8">
-        <nav className="bg-gray-900 backdrop-filter backdrop-blur-sm nav-shadow bg-opacity-50 border-b border-gray-600 mx-0 md:mx-auto px-4 py-2 max-w-3xl rounded-full">
+        <nav className="bg-gray-900 backdrop-filter backdrop-blur-sm nav-shadow bg-opacity-50 border-b border-gray-600 mx-0 md:mx-auto px-2 py-2 max-w-3xl rounded-full">
           <div className="flex items-center justify-between">
             <img src="/favicon.svg" alt="favicon" />
-            <div className="flex gap-2 lg:gap-8 items-center text-sm">
-              <ul className="text-portfolioWhite flex gap-2 lg:gap-4">
+              <ul className="text-portfolioWhite flex gap-4">
                 <li className="border-b-2 border-portfolioWhite py-1">
                   <a href="#" aria-current className="font-semibold">Home</a>
                 </li>
@@ -33,26 +32,28 @@ export default function NavBar() {
                   <a className="text-gray-300" href="#about">About</a>
                 </li>
               </ul>
+            <div className="flex gap-2 lg:gap-8 items-center text-sm">
 
-              <button
-                className="button-shadow bg-portfolioSecondary text-sm font-semibold leading-6 border border-[#00428C] text-white py-2 px-4 rounded-full"
-                onClick={handleButtonClick}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                Say hi{" "}
-                {isHovered ? (
-                  <img
-                    style={{ display: "inline", verticalAlign: "middle" }}
-                    width="14px"
-                    height="14px"
-                    src={waveGif}
-                    alt="waving hand gif"
-                  />
-                ) : (
-                  "👋"
-                )}
-              </button>
+            <button
+  className="button-shadow bg-portfolioSecondary text-sm font-semibold leading-6 border border-[#00428C] text-white py-2 px-4 rounded-full"
+  onClick={handleButtonClick}
+  onMouseEnter={() => setIsHovered(true)}
+  onMouseLeave={() => setIsHovered(false)}
+>
+  <span className="say-hi-text">Say hi </span>
+  {isHovered ? (
+    <img
+      style={{ display: "inline", verticalAlign: "middle" }}
+      width="14px"
+      height="14px"
+      src={waveGif}
+      alt="waving hand gif"
+    />
+  ) : (
+    "👋"
+  )}
+</button>
+
             </div>
           </div>
         </nav>
