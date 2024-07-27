d4c4af01b0e1198c9fe6cfeadb211179310d6c77
Mahama Hamida <mahamahamida@gmail.com>
2024-07-27 00:24:32 +0000

    refactor: Update Card component to use thumbnailUrl instead of baseUrl for image source

--- MyPortfolio/src/components/Card.jsx
+++ MyPortfolio/src/components/Card.jsx
@@ -9,12 +9,12 @@ import PropTypes from "prop-types";
     const [isModalOpen, setModalOpen] = useState(false);
     const openModal = () => setModalOpen(true);
     const closeModal = () => setModalOpen(false);
-    const baseUrl = "https://hamida-mahama.onrender.com";
-  // const baseUrl = "http://localhost:1337";
+    // const baseUrl = "https://hamida-mahama.onrender.com";
+  const baseUrl = "http://localhost:1337";
 
-    const { title, thumbnail, projectVideo, description, id, tags } = project;
+    const { title, description, id, tags,thumbnailUrl,thumbnailGifUrl } = project;
     // console.log(project.description[0]?.children[0].text);
-    console.log(project.tags?.projectTags);
+    console.log(project.title);
   return (
     <>
       <div
@@ -22,11 +22,11 @@ import PropTypes from "prop-types";
         className="bg-gray-600 max-w-sm w-full max-h-64 h-auto rounded-3xl overflow-hidden mb-4"
         onClick={openModal}
       >
-        {thumbnail ? (
+        {thumbnailUrl ? (
           <img
             className="w-full h-full object-cover transition-transform duration-200 hover:scale-105"
-            src={baseUrl + project.thumbnail.url}
-            alt={baseUrl + project.thumbnail.altText || 'Thumbnail'}
+            src={project.thumbnailUrl}
+            // alt={baseUrl + project.thumbnail.altText || 'Thumbnail'}
           />
         ) : (
           <div className="w-full h-full flex items-center justify-center text-white">
@@ -34,7 +34,7 @@ import PropTypes from "prop-types";
           </div>
         )}
       </div>
-      <ProductModal isOpen={isModalOpen} onClose={closeModal} projectTitle={project.title} projectDescription={project.description[0]?.children[0].text} projectVideo={baseUrl + project.projectVideo.url} projectTags={project.tags?.projectTags}  />
+      <ProductModal isOpen={isModalOpen} onClose={closeModal} projectTitle={project.title} projectDescription={project.description[0]?.children[0].text} projectTags={project.tags?.projectTags} thumbnailGifUrl={project.thumbnailGifUrl}  />
     </>
   );
 }
