a85a04beb49195db7050ff2ed9d7dccb756893ad
Mahama Hamida <mahamahamida@gmail.com>
2024-07-27 00:25:42 +0000

    refactor: Update API fetchProjects function to remove unused code and comments

--- MyPortfolio/src/lib/api.js
+++ MyPortfolio/src/lib/api.js
@@ -2,23 +2,23 @@ import axios from "axios";
 import qs from "qs";
 import { flattenAttributes } from "./utils/utils";
 
-const projectQuery = qs.stringify({
-  populate: {
-    thumbnail: {
-      fields: ["url", "altText"],
-    },
-    projectVideo: {
-      fields: ["url", "altText"],
-    },
-  },
-});
+// const projectQuery = qs.stringify({
+//   populate: {
+//     thumbnail: {
+//       fields: ["url", "altText"],
+//     },
+//     projectVideo: {
+//       fields: ["url", "altText"],
+//     },
+//   },
+// });
 
 const fetchProjects = async (path) => {
   const baseUrl = "https://hamida-mahama.onrender.com/";
   // const baseUrl = "http://localhost:1337";
 
   const url = new URL(path, baseUrl);
-  url.search = projectQuery;
+  // url.search = projectQuery;
   console.log(url.href);
 
   try {
