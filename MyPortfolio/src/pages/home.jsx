4e703654a33e393620088beef42f2d554f302c8d
Mahama Hamida <mahamahamida@gmail.com>
2024-08-01 01:09:15 +0000

    refactor: Add Home component for homepage layout

---
+++ MyPortfolio/src/pages/home.jsx
@@ -0,0 +1,16 @@
+
+import Works from '../components/Works.jsx'
+import About from '../components/About.jsx'
+import HeroSection from '../components/Hero.jsx'
+
+export default function Home() {
+    return(
+    <>
+    
+    <HeroSection />
+    <Works />
+    {/* <div className="elfsight-app-15429149-2614-4ee4-b451-493b151f462d" data-elfsight-app-lazy></div> */}
+    <About />
+    </>
+    )
+}
