4a155836830501d957a483c37f8d11321a28ea82
Mahama Hamida <mahamahamida@gmail.com>
2024-08-01 01:08:03 +0000

    refactor: Update App component to use Router for navigation

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
-import About from './components/About.jsx'
-import HeroSection from './components/Hero.jsx'
-import './components/NavBar.jsx'
 import NavBar from './components/NavBar.jsx'
-import Works from './components/Works.jsx'
+
+import Router from './Router.jsx'
 
 function App() {
   return (
     <>
     <NavBar />
-    <HeroSection />
-    <Works />
-    {/* <div class="elfsight-app-15429149-2614-4ee4-b451-493b151f462d" data-elfsight-app-lazy></div> */}
-    <About />
+    <Router />
+   
     </>
   )
 }
