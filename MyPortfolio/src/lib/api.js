968371447a697dc946b3a710c5a9f6a6b641d69d
Mahama Hamida <mahamahamida@gmail.com>
2024-08-04 23:10:52 +0000

    refactor: Add lazy loading for images in main.jsx

--- MyPortfolio/src/lib/api.js
+++ MyPortfolio/src/lib/api.js
@@ -1,24 +1,13 @@
 import axios from "axios";
-import qs from "qs";
 import { flattenAttributes } from "./utils/utils";
 
-// const projectQuery = qs.stringify({
-//   populate: {
-//     thumbnail: {
-//       fields: ["url", "altText"],
-//     },
-//     projectVideo: {
-//       fields: ["url", "altText"],
-//     },
-//   },
-// });
+
 
 const fetchProjects = async (path) => {
   // const baseUrl = "https://hamida-mahama.onrender.com/";
   const baseUrl = "http://localhost:1337";
 
   const url = new URL(path, baseUrl);
-  // url.search = projectQuery;
   console.log(url.href);
 
   try {
@@ -29,5 +18,7 @@ const fetchProjects = async (path) => {
     console.error(err);
   }
 };
+
+
 
 export { fetchProjects };
