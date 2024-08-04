b79178d1010dbcbc6a4e10d113b3a4588cd6fac4
Mahama Hamida <mahamahamida@gmail.com>
2024-08-04 23:09:53 +0000

    refactor: Update About component to fetch and display dynamic stories

--- MyPortfolio/src/components/About.jsx
+++ MyPortfolio/src/components/About.jsx
@@ -1,20 +1,40 @@
-import React, { useState, useEffect } from "react";
-import me from "../assets/thumbnails/hamida.jpg";
-import mee from "../assets/thumbnails/hamida2.jpg";
-import meee from "../assets/thumbnails/hamida3.jpg";
-import meeee from "../assets/thumbnails/hamida5.jpg";
+import React, { useState, useEffect, Fragment } from "react";
+import first from "../assets/thumbnails/hamida.jpg";
+import second from "../assets/thumbnails/hamida2.jpg";
+import third from "../assets/thumbnails/hamida3.jpg";
+import fourth from "../assets/thumbnails/hamida5.jpg";
+import { fetchProjects } from "../lib/api";
 
 export default function About() {
-  const images = [me, mee, meee, meeee];
-  const [currentImage, setCurrentImage] = useState(0);
+  const [currentStory, setCurrentStory] = useState(0);
+  const [fetchedAbout, setFetchedAbout] = useState([]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 3000);
+    const getAbout = async () => {
+      const fetchedAboutData = await fetchProjects("/api/about");
+      const stories = fetchedAboutData.story.content;
+      console.log(stories)
+      setFetchedAbout(stories);
+    };
 
-    return () => clearInterval(interval);
-  }, [images.length]);
+    getAbout();
+  }, []);
+
+  useEffect(() => {
+    if (fetchedAbout.length > 0) {
+      const interval = setInterval(() => {
+        setCurrentStory((prevStory) => (prevStory + 1) % fetchedAbout.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [fetchedAbout]);
+
+  if (fetchedAbout.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+ 
 
   return (
     <section
@@ -24,19 +44,27 @@ export default function About() {
       <div className="flex gap-8 flex-col max-w-3xl mx-auto justify-center items-center">
         <p className="text-3xl xl:text-[40px] font-bold">About Me 😉</p>
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 w-full">
+          
+        
+            
           <div className="border border-gray-400 sticker-shadow rounded-3xl bg-white w-full h-auto max-h-72 overflow-hidden flex justify-center items-center -rotate-3">
             <img
-              src={images[currentImage]}
+         
+              src={fetchedAbout[currentStory].image}
               className="object-contain"
               alt="About me"
-            />
+              />
           </div>
           <div className="max-w-sm text-left w-full mb-4">
-            <p className="inline-paragraph">
-              Whoa, you got this far?! Your interest means a lot to me
+            <p className={currentStory === 0 ? "inline-paragraph" : ""}>
+            {fetchedAbout[currentStory].text}
+              
             </p>
-            <p>Hamida Mahama is my name. </p>
+          
           </div>
+        
+              
+      
         </div>
       </div>
     </section>
